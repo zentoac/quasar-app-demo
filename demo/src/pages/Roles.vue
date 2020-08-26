d9048d866ed7ffa66eb97e9aca2f62ad0382ed2e
egallerani <nulele@gmail.com>
2020-08-26 15:18:32 +0200

    adding static content to users and roles pages

--- demo/src/pages/Roles.vue
+++ demo/src/pages/Roles.vue
@@ -1,6 +1,32 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Roles</h1>
+  <q-page class="">
+    <q-list bordered separator class="rounded-borders">
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label>Account</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label>Graphic Designer</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label>Project Manager</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label>Junior Programmer</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section>
+          <q-item-label>Senior Programmer</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
--- demo/src/pages/Users.vue
+++ demo/src/pages/Users.vue
@@ -1,6 +1,61 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Users</h1>
+  <q-page class="">
+    <q-list bordered separator class="rounded-borders">
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <img height="35" width="35" src="~assets/albert.png" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Albert</q-item-label>
+          <q-item-label caption>Junior Programmer</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <img height="35" width="35" src="~assets/brian.png" >
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Brian</q-item-label>
+          <q-item-label caption>Junior Programmer</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <img height="35" width="35" src="~assets/john.png" >
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>John</q-item-label>
+          <q-item-label caption>Project Manager</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <img height="35" width="35" src="~assets/kate.png" >
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Kate</q-item-label>
+          <q-item-label caption>Graphic Designer</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <img height="35" width="35" src="~assets/mark.png" >
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Mark</q-item-label>
+          <q-item-label caption>Senior Programmer</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <img height="35" width="35" src="~assets/sharon.png" >
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>Sharon</q-item-label>
+          <q-item-label caption>Account</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
