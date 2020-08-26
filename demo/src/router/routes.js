bb7690708652aec23d11fb65d294d144668c16f3
egallerani <nulele@gmail.com>
2020-08-26 13:26:42 +0200

    creating users and roles pages with basic rounting

--- demo/src/router/routes.js
+++ demo/src/router/routes.js
@@ -4,7 +4,9 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/users', component: () => import('pages/Users.vue') },
+      { path: '/roles', component: () => import('pages/Roles.vue') },
     ]
   },
 
