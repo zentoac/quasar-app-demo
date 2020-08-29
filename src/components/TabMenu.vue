a1d13ee1853f479924335896676552794effbde5
egallerani <nulele@gmail.com>
2020-08-29 21:15:11 +0200

    replacing drawer menu with tabs menu

---
+++ src/components/TabMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-tabs dense inline-label>
+    <q-route-tab
+      v-for="(menu, index) in menus"
+      :key="index"
+      :icon="menu.icon"
+      :to="menu.to"
+      :label="menu.title"
+      exact
+    />
+  </q-tabs>
+</template>
+
+<script>
+export default {
+  name: 'TabMenu',
+  data() {
+    return {
+
+    }
+  },
+  props: [
+    'menus'
+  ]
+}
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,55 +2,36 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
 
         <q-toolbar-title>
           Quasar Demo App
         </q-toolbar-title>
 
-        <div></div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Menu
-        </q-item-label>
-        <EssentialRouteItem
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+    <q-footer elevated>
+      <tab-menu :menus="menus" />
+    </q-footer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
   </q-layout>
 </template>
 
 <script>
 
-import EssentialRouteItem from "components/EssentialRouteItem";
+import TabMenu from "components/TabMenu";
 
-const linksData = [
+const menus = [
+  {
+    title: 'Home',
+    caption: 'Home',
+    icon: 'house',
+    to: '/'
+  },
   {
     title: 'Users',
     caption: 'Users listing',
@@ -67,11 +48,10 @@ const linksData = [
 
 export default {
   name: 'MainLayout',
-  components: {EssentialRouteItem},
+  components: {TabMenu},
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+      menus: menus
     }
   }
 }
