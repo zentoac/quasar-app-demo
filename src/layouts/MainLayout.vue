827682d7877cf8a00c73442fad93f8c6f858f926
egallerani <nulele@gmail.com>
2020-09-03 12:50:16 +0200

    checking network information

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -18,13 +18,20 @@
       </q-toolbar>
     </q-header>
 
-    <q-footer elevated>
-      <tab-menu :menus="menus" />
-    </q-footer>
+    <div v-if="!offline">
+      <q-footer v-if="!offline" elevated>
+        <tab-menu :menus="menus" />
+      </q-footer>
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
+      <q-page-container>
+        <router-view  />
+      </q-page-container>
+    </div>
+
+    <div v-if="offline" class="flex fullscreen justify-center content-center bg-red-2">
+      <div class="text-h3 q-pa-md">You are offline!</div>
+      <div class="text-h5">Check your connection.</div>
+    </div>
 
   </q-layout>
 </template>
@@ -59,8 +66,12 @@ export default {
   components: {TabMenu},
   data () {
     return {
+      offline: false,
       menus: menus
     }
+  },
+  created() {
+    this.checkNetwork()
   },
   methods: {
     pageTitle() {
@@ -71,6 +82,16 @@ export default {
     },
     goBack() {
       this.$router.go(-1);
+    },
+    checkNetwork() {
+      // check if online
+      window.addEventListener('offline', () => {
+        this.offline = true;
+      });
+      // check if offline
+      window.addEventListener('online', () => {
+        this.offline = false;
+      });
     }
   }
 }
